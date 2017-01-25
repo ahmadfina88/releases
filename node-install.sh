2f8284c70026dddef9d40e19c2e7bebefbea4361
DigitalRs <malcifra@gmail.com>
2017-01-25 15:50:00 +0500

    format the bin installer

--- node-install.sh
+++ node-install.sh
@@ -2,8 +2,11 @@
 
 echo "DIGITALRS INSTALLER"
 
-echo "Must install dependencies"
+echo "Must install dependencies first..."
+
 sudo apt-get install build-essential libssl-dev libdb++-dev libboost-all-dev libqrencode-dev libminiupnpc-dev unrar
+
+echo "Downloading the stripped binary from github..."
 
 wget https://github.com/DigitalRs/releases/blob/master/digitalrsd-binary-deb.rar
 
@@ -13,13 +16,18 @@ mkdir .digitalrs
 
 cd .digitalrs
 
+echo "Downloading the chain data..."
+
 wget https://github.com/DigitalRs/testing/blob/master/bootstrap.rar
+
 unrar x bootstrap.rar
 
-echo "rpcuser=user\nrpcpassword=password\nrpcallowip=127.0.0.1\ndaemon=1\nserver=1" > digitalrs.conf
+echo "rpcuser=user\nrpcpassword=password\nrpcallowip=127.0.0.1\ndaemon=1\nserver=1\naddnode=203.128.6.219" > digitalrs.conf
 
 cd ..
 
 chmod +x ./digitalrsd
 
-./digitalrsd
+mv digitalrsd /usr/bin
+
+digitalrsd
